8b1e8abf32d4a2006c15ea6ff1322bd512563069
UglystephanHestonovich <127040306+UglystephanHestonovich@users.noreply.github.com>
2023-03-15 10:05:00 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['POST'])
+def hello_world():
+    a = 0
+    b = list(request.json['data'])
+    for i in range(len(b)):
+        a += b[i]
+
+    c = {'sum':a}
+    return c
+
+app.run()
